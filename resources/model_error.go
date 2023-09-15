64a9d9c1a32d03cb25a0a43118d8ba611ae71d37
lukachi <lukachisama@gmail.com>
2023-09-15 18:05:56 +0300

    generate models from docs

---
+++ resources/model_error.go
@@ -0,0 +1,11 @@
+/*
+ * GENERATED. Do not modify. Your changes might be overwritten!
+ */
+
+package resources
+
+// Standard JSON:API error
+type ModelError struct {
+	// Non-empty array of errors occurred during the request processing
+	Errors []ErrorErrors `json:"errors"`
+}
